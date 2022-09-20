3e6a656d1c6bfb37d8ded2febf0139185f81c7fd
limilano <limilano@davidson.edu>
2022-09-20 18:13:29 -0400

    bg image added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,16 @@
 
 /* javascript */
+
+// $("h1").hide();
+
+$(".clickme").click(function(e){
+  console.log("clicked!",e);
+});
+
+let username = "";
+
+$("form").submit(function(e){
+  e.preventDefault();
+  username = $(".username").val();
+  console.log(username);
+});
