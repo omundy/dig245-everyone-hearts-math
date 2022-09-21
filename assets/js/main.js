79e733a0cbcc3cd5e6e9990119c4eb7fef92ac12
limilano <limilano@davidson.edu>
2022-09-21 15:48:16 -0400

    incomplete logic for multi table

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,31 @@ $(".clickme").click(function(e){
   console.log("clicked!",e);
 });
 
-let username = "";
+// let username = "";
+
+let str = "";
+
+let answers = [[1,2,3,4,5,6,7,8,9,10],
+              [2,4,6,8,10,12,14,16,18,20]];
 
 $("form").submit(function(e){
   e.preventDefault();
-  username = $(".username").val();
-  console.log(username);
+  // create array of all answers
+  console.log("submitted");
+  let input = [[$(".a1").val(),$(".a2").val()],[]];
+  console.log(input);
+  for(let row = 0; row < 11; row++) {
+    for(let col = 0; col<11;col++) {
+      if(answers[row][col] == input[row][col]) {
+        console.log(answers[row][col], input[row][col], "correct!");
+      } else {
+        console.log(answers[row][col], input[row][col],"incorrect");
+      }
+    }
+  }
+
+  // username = $(".username").val();
+  // console.log(username);
 });
+
+$(".output").html(str);
