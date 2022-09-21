bcf7b410a8b5fbfd32151d26c26c60f4797633d6
KostasMateer <komateer@davidson.edu>
2022-09-21 09:16:50 -0400

    update index.html, main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
-
 /* javascript */
+//$("h1").hide();
+
+let username = "";
+let requiredUsername = "secret";
+
+$("form").submit(function (e) {
+  e.preventDefault();
+  username = $(".username").val();
+  console.log("username is " + username);
+
+
+  let str = " ";
+  if (username == requiredUsername){
+    console.log("nice");
+    str = `
+    <div class="alert alert-success" role="alert">
+      welcome, friend ${username}
+    </div>
+    `;
+  }
+
+  $('.output').html(str);
+});
