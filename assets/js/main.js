3febec8fd3f1d6c7446fe124bc7c1d5030cee3b3
Pauline Cha <pacha@davidson.edu>
2022-09-25 14:10:47 -0400

    Reload: Visual Elements- heading, font, street visual, word problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,35 @@
-
 /* javascript */
+
+// $("h1").hide();
+$(".clickme").click(function(eventObject){
+  console.log("hello!", eventObject);
+});
+
+let username = "";
+let requiredUsername = "secret";
+
+$("form").submit(function(e){
+  e.preventDefault();
+  username = $(".username").val();
+  console.log(username);
+
+
+let str = "";
+
+if (username == requiredUsername){
+  str = `
+  <div class="alert alert-success" role="alert">
+  Welcome, friend.
+  </div>`;
+}
+else {
+  str = `
+  <div class="alert alert-danger" role="alert">
+  Not allowed.
+  </div>`;
+}
+
+
+$(".output").html(str);
+
+});
